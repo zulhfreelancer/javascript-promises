5234b9958b35d5a03334667ffba896310794cc6d
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-04-28 14:39:21 +0000

    Add ES6 examples into its folder

---
+++ 2-es6/mom-and-phone.js
@@ -0,0 +1,39 @@
+const isMomHappy = true;
+
+// promise 1
+const willIGetNewPhone = new Promise((resolve, reject) => {
+  if (isMomHappy) {
+    const phone = {
+      brand: 'iPhone X',
+      color: 'Black'
+    };
+    console.log("resolving willIGetNewPhone promise");
+    resolve(phone); // fullfilled
+  } else {
+    const reason = new Error('Mom is not happy');
+    console.log("rejecting willIGetNewPhone promise")
+    reject(reason);
+  }
+});
+
+// promise 2
+const showOff = function(phone) {
+  return new Promise(function(resolve, reject) {
+    const message = 'Hey, I have a new ' + phone.color + ' ' + phone.brand + ' phone.';
+    resolve(message);
+  });
+};
+
+// ES6 way
+const askMom = function() {
+  willIGetNewPhone
+    .then(showOff)
+    .then((fullfilled) => {
+      console.log("success", fullfilled);
+    })
+    .catch((error) => {
+      console.log("error", error.message);
+    })
+}
+
+askMom();
